addf004c25cab0f3934f3d52494f4b8a6304b92e
Adrian-2507 <adriang2507@gmail.com>
2025-09-01 17:35:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,26 @@ const navLinks = document.querySelector('.nav-links');
 menuToggle.addEventListener('click', () => {
   navLinks.classList.toggle('show');
 });
+
+/* js/script.js */
+// Responsive Navbar
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('show');
+});
+
+// Hero Background Slider
+const hero = document.querySelector('.hero');
+const heroImages = [
+  'assets/images/hero1.jpg',
+  'assets/images/hero2.jpg',
+  'assets/images/hero3.jpg'
+];
+let currentHero = 0;
+function changeHeroBackground() {
+  currentHero++;
+  if (currentHero >= heroImages.length) currentHero = 0;
+  hero.style.backgroundImage = `url('${heroImages[currentHero]}')`;
+}
+setInterval(changeHeroBackground, 5000);
